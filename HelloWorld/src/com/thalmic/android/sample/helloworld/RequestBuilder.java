98f530e8945868d952f15f7b4e16a75460a2f78b
everfor <alan.yang1994@gmail.com>
2014-09-27 18:03:48 -0400

    Minor modifications and comments for request builder

--- HelloWorld/src/com/thalmic/android/sample/helloworld/RequestBuilder.java
+++ HelloWorld/src/com/thalmic/android/sample/helloworld/RequestBuilder.java
@@ -17,33 +17,35 @@ import android.os.AsyncTask;
 import android.os.StrictMode;
 
 public class RequestBuilder extends AsyncTask<String, Void, JSONObject> {
+	// API call parameter list
     private static final String HEADER = "https://maps.googleapis.com/maps/api/directions/json?";
     private static final String ORIGIN = "origin=";
     private static final String DEST = "destination=";
     private static final String KEY = "key=";
-    private static final String ALTROUTE = "alternatives";
-    private static final String TRUE = "true";
     private static final String DEPARTURE_TIME = "departure_time=";
+    private static final String MODE = "mode=";
+    // Value of api parameters
     private static final String UNITS = "units";
     private static final String METRIC = "metric";
-    private static final String MODE = "mode=";
+    private static final String ALTROUTE = "alternatives";
+    private static final String TRUE = "true";
     private static final String WALK = "walking";
     private static final String API_KEY = "AIzaSyA3bq0V_IMiwMGL4MR3M-1LmdbVB8kB-0k";
     
+    // Stores result directions from api call
     public static JSONObject Directions = null;
     
     private static String getCurrentTime() {
         return Integer.toString((int)(System.currentTimeMillis() / 1000));
     } 
     
+    // Returns json string of direction api call results
     private String getDirectionString(String origin, String destination) {
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-        
         HttpClient client = new DefaultHttpClient();
         HttpResponse response;
         
         try {
+        	// Use http get to get directions
             response = client.execute(new HttpGet(RequestBuilder.buildRequestURL(origin, destination)));
             
             StatusLine status = response.getStatusLine();
@@ -68,9 +70,10 @@ public class RequestBuilder extends AsyncTask<String, Void, JSONObject> {
             return e.toString();
         }
         
-        return "Empty";
+        return null;
     }
     
+    // Parses json string returned from directions api into json object
     private JSONObject getDirections(String origin, String destination) {
         String directionString = this.getDirectionString(origin, destination);
         
@@ -96,16 +99,21 @@ public class RequestBuilder extends AsyncTask<String, Void, JSONObject> {
                 + RequestBuilder.MODE + RequestBuilder.WALK;
     }
     
-    public static void resetDirections() {
+    // Reset directions data to make it ready for next use
+    public static void ResetDirections() {
     	RequestBuilder.Directions = null;
     }
 
     @Override
     protected JSONObject doInBackground(String... place) {
-    	RequestBuilder.Directions = null;
+    	// Reset direction data to be safe
+    	RequestBuilder.ResetDirections();
     	
         if (place.length != 2)
             return null;
+        
+        // place[0] - origin
+        // place[1] - destination
         return this.getDirections(place[0], place[1]);
     }
     
