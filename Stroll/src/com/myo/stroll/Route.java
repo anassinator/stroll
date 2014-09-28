a50a8fd7d1fac2f455d633224d72205f57877275
Anass Al <anass.alwohoush@outlook.com>
2014-09-28 09:17:25 -0400

    Add route distance

--- Stroll/src/com/myo/stroll/Route.java
+++ Stroll/src/com/myo/stroll/Route.java
@@ -2,8 +2,12 @@ package com.myo.stroll;
 
 public class Route {
 	Step[] steps;
+	int distance = 0;
 	
 	public Route(Step[] steps) {
 		this.steps = steps;
+		for (int i = 0; i < steps.length; i++) {
+			this.distance += steps[i].distance;
+		}
 	}
 }
